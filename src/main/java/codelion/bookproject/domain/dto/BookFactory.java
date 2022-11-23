78e90701c8b5639f4562da783904fa3d45bcea1d
seoyun <may-primavera@naver.com>
2022-11-23 13:46:47 +0900

    feat: book response factory

---
+++ src/main/java/codelion/bookproject/domain/dto/BookFactory.java
@@ -0,0 +1,13 @@
+package codelion.bookproject.domain.dto;
+
+import codelion.bookproject.domain.entity.Books;
+
+public class BookFactory {
+
+    public static BookResponse toBookResponse(Books books) {
+        return new BookResponse(
+                  books.getId()
+                , books.getName()
+                , books.getAuthor().getAuthorName());
+    }
+}
